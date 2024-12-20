fe8e66e8305f6054d01b47e2fdf720bdd3ea62ab
xyryc <amdanikzz@gmail.com>
2024-12-20 18:36:43 +0600

    added field reset functionality

--- client/src/pages/AllJobs.jsx
+++ client/src/pages/AllJobs.jsx
@@ -22,7 +22,11 @@ const AllJobs = () => {
     fetchAllJobs();
   }, [filter, search, sort]);
 
-  console.log(sort);
+  const handleReset = () => {
+    setFilter("");
+    setSearch("");
+    setSort("");
+  };
 
   return (
     <div className="container px-6 py-10 mx-auto min-h-[calc(100vh-306px)] flex flex-col justify-between">
@@ -34,6 +38,7 @@ const AllJobs = () => {
               id="category"
               className="border p-4 rounded-lg"
               onChange={(e) => setFilter(e.target.value)}
+              value={filter}
             >
               <option value="">Filter By Category</option>
               <option value="Web Development">Web Development</option>
@@ -52,6 +57,7 @@ const AllJobs = () => {
               onChange={(e) => {
                 setSearch(e.target.value);
               }}
+              value={search}
             />
 
             <button className="px-1 md:px-4 py-3 text-sm font-medium tracking-wider text-gray-100 uppercase transition-colors duration-300 transform bg-gray-700 rounded-md hover:bg-gray-600 focus:bg-gray-600 focus:outline-none">
@@ -67,13 +73,16 @@ const AllJobs = () => {
               onChange={(e) => {
                 setSort(e.target.value);
               }}
+              value={sort}
             >
               <option value="">Sort By Deadline</option>
               <option value="dsc">Descending Order</option>
               <option value="asc">Ascending Order</option>
             </select>
           </div>
-          <button className="btn">Reset</button>
+          <button onClick={handleReset} className="btn">
+            Reset
+          </button>
         </div>
         <div className="grid grid-cols-1 gap-8 mt-8 xl:mt-16 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
           {jobs.map((job) => (
